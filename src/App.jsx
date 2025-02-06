2c14731731c9dc93855be38a84a36ba897169a1f
Pabindu <pabinduamodya12@gmail.com>
2025-02-06 19:51:47 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ProductCard from './components/productCard.jsx'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ProductCard/>
+      <ProductCard/>
+      <ProductCard/>
+
+      
+    </>
+  )
+}
+
+export default App
