ddf72e32b89f9a34eb51c47285058184eedb8382
Pabindu <pabinduamodya12@gmail.com>
2025-02-07 12:05:02 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ProductCard from './components/productCard.jsx'
+import Product from './components/procard.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <ProductCard/>
-    
-      
-
-      
+    <Product/>
+  
     </>
   )
 }
---
+++ src/components/procard.jsx
@@ -0,0 +1,11 @@
+import "./product.css"
+export default function Product(){
+    return (
+        <div className="product-card">
+            <h1>Product One</h1>
+            <h2>Price: $100</h2>
+            <button>Add to cart</button>
+        </div>
+    );
+
+}
