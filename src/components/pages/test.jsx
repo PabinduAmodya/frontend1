cd8824b7ddb101b511c060bfee816855e06d4c8d
Pabindu <pabinduamodya12@gmail.com>
2025-02-17 10:08:18 +0530

    error fix😮‍💨

--- src/components/pages/test.jsx
+++ src/components/pages/test.jsx
@@ -1,11 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import { useState } from "react"
 
-const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpYXFmYXRudnNxaGxwaXBpa
-GFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1OTY1MTE
-sImV4cCI6MjA1NTE3MjUxMX0.ZjXdyfRJXw1RZ2vqftjlF
-jRG7c6zltrUzV9tFXstfs0`
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpYXFmYXRudnNxaGxwaXBpaGFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1OTY1MTEsImV4cCI6MjA1NTE3MjUxMX0.ZjXdyfRJXw1RZ2vqftjlFjRG7c6zltrUzV9tFXstfs0
+
+
+`
 
 const url="https://biaqfatnvsqhlpipihaf.supabase.co"
 
@@ -21,7 +20,7 @@ export default function FileUploadTest(){
         console.log(file);
 
         const fileName = file.name
-        const extension =fileName.split(".")[fileName.split(".".length)]
+        const extension =fileName.split(".")[fileName.split(".").length-1]
 
         if(extension != "jpg" && extension != "png"){
             alert("Please select a jpg or png file")
@@ -33,6 +32,9 @@ export default function FileUploadTest(){
         supabase.storage.from("images").upload(file.name,file,{
             cacheControl : "3600",
             upsert : false
+        }).then((res)=>{
+            console.log(res)
+            
         })
     }
 
