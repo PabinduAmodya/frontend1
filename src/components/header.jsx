504a4e84bfb8a9bec75763cbf8393986752d03aa
Pabindu <pabinduamodya12@gmail.com>
2025-02-18 02:01:24 +0530

    update header🤷‍♂️

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,14 +10,35 @@ export default function Header() {
                 className="cursor-pointer w-[60px] h-[60px] rounded-full absolute left-4 top-1/2 -translate-y-1/2"
             />
 
-            {/* Navigation Links */}
+            {/* Navigation Links with Underline Animation */}
             <nav className="w-full flex justify-center space-x-8 text-gray-800 font-medium text-lg">
-                <Link to="/" className="hover:text-gray-600 transition">Home</Link>
-                <Link to="/products" className="hover:text-gray-600 transition">Products</Link>
-                <Link to="/about" className="hover:text-gray-600 transition">About Us</Link>
-                <Link to="/contact" className="hover:text-gray-600 transition">Contact Us</Link>
+                <Link to="/" className="relative hover:text-gray-600 transition duration-300 
+                                       after:block after:h-[2px] after:bg-gray-600 after:w-full 
+                                       after:scale-x-0 hover:after:scale-x-100 after:transition-transform 
+                                       after:duration-300 after:origin-left">
+                    Home
+                </Link>
+                <Link to="/products" className="relative hover:text-gray-600 transition duration-300 
+                                                after:block after:h-[2px] after:bg-gray-600 after:w-full 
+                                                after:scale-x-0 hover:after:scale-x-100 after:transition-transform 
+                                                after:duration-300 after:origin-left">
+                    Products
+                </Link>
+                <Link to="/about" className="relative hover:text-gray-600 transition duration-300 
+                                             after:block after:h-[2px] after:bg-gray-600 after:w-full 
+                                             after:scale-x-0 hover:after:scale-x-100 after:transition-transform 
+                                             after:duration-300 after:origin-left">
+                    About Us
+                </Link>
+                <Link to="/contact" className="relative hover:text-gray-600 transition duration-300 
+                                               after:block after:h-[2px] after:bg-gray-600 after:w-full 
+                                               after:scale-x-0 hover:after:scale-x-100 after:transition-transform 
+                                               after:duration-300 after:origin-left">
+                    Contact Us
+                </Link>
             </nav>
         </header>
     );
 }
+
 
