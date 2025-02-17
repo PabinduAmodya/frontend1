de8f5a1405d253a67f3f5b18a0ad08fac65bc2f2
Pabindu <pabinduamodya12@gmail.com>
2025-02-17 09:46:31 +0530

    restricted file type🫠🫠

--- src/components/pages/test.jsx
+++ src/components/pages/test.jsx
@@ -1,4 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
 import { useState } from "react"
+
+const key = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJpYXFmYXRudnNxaGxwaXBpa
+GFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1OTY1MTE
+sImV4cCI6MjA1NTE3MjUxMX0.ZjXdyfRJXw1RZ2vqftjlF
+jRG7c6zltrUzV9tFXstfs0`
+
+const url="https://biaqfatnvsqhlpipihaf.supabase.co"
+
 
 export default function FileUploadTest(){
 
@@ -9,6 +19,21 @@ export default function FileUploadTest(){
             return;
         }
         console.log(file);
+
+        const fileName = file.name
+        const extension =fileName.split(".")[fileName.split(".".length)]
+
+        if(extension != "jpg" && extension != "png"){
+            alert("Please select a jpg or png file")
+            return
+        }
+
+        const supabase = createClient(url,key)
+
+        supabase.storage.from("images").upload(file.name,file,{
+            cacheControl : "3600",
+            upsert : false
+        })
     }
 
     return(
