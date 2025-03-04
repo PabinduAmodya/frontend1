8fe30aa397c04879c6b9504edcef6a3848faf07a
Pabindu <pabinduamodya12@gmail.com>
2025-03-04 12:27:18 +0530

    admin autharzation

--- src/components/pages/adminhome.jsx
+++ src/components/pages/adminhome.jsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import { MdDashboard } from "react-icons/md";
 import { FaBoxArchive } from "react-icons/fa6";
 import { FaPencilAlt, FaShoppingCart } from "react-icons/fa";
@@ -7,6 +7,9 @@ import AdminProductPAge from "./admin/adminProductPage";
 import AddProductPage from "./admin/addProduct";
 import EditProduct from "./admin/editProducts";
 import AdminOrdersPage from "./admin/adminOrder";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 
 
@@ -16,6 +19,35 @@ import AdminOrdersPage from "./admin/adminOrder";
 
 
 export default function AdminHomePage() {
+    const [user,setUser] = useState(null)
+  const navigate = useNavigate();
+  useEffect(()=>{
+    const token = localStorage.getItem("token");
+    if (!token) {      
+      navigate("/login")
+      return;
+    }
+    axios
+      .get(import.meta.env.VITE_BACKEND_URL + "/api/users", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((res)=>{
+        console.log(res.data)
+        if(res.data.type!="admin"){
+          toast.error("Unauthorized access")
+          navigate("/login")
+        }else{
+          setUser(res.data)
+        }
+
+      }).catch((err)=>{
+        console.error(err)
+        toast.error("Failed to fetch user data")
+        navigate("/login")
+      })
+    },[])
+
     return (
         <div className="bg-blue-300 w-full min-h-screen flex">
             {/* Sidebar */}
@@ -44,14 +76,20 @@ export default function AdminHomePage() {
             
             {/* Main Content */}
             <div className="w-[80%] min-h-screen bg-blue-200 overflow-auto">
-                <Routes>
+                {user!=null && <Routes path="/*">
                     <Route path="/dashboard" element={<h1>Dashboard</h1>} />
                     <Route path="/products" element={<AdminProductPAge/>} />
                     <Route path="/products/addProduct" element={<AddProductPage/>}/>
                     <Route path="/orders" element={<AdminOrdersPage/>} />
                     <Route path="/products/edit" element={<EditProduct/>} />
                     <Route path="/*" element={<h1>404 NOT FOUND</h1>} />
-                </Routes>
+                </Routes>}
+                {
+                    user==null&&<div className="w-full h-full flex justify-center items-center">
+                         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
+
+                    </div>
+                }
             </div>
         </div>
     );
